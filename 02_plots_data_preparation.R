ece15268e4bd5b15b3c194f7af4753ed25c9326e
Javier Atalah <jatalah@gmail.com>
2024-08-23 12:47:00 +1200

    v3

--- 02_plots_data_preparation.R
+++ 02_plots_data_preparation.R
@@ -32,12 +32,25 @@ read_rds_file_metadata <- function(file_path) {
 }
 
 # List .rds files containing "PlotSmall" in their names
-plot_small_files <- dir_ls(folder_path, recurse = TRUE, regexp = ".*PlotSmall.*\\.rds$")
+node_status_plots_files <- dir_ls(folder_path, recurse = TRUE, regexp = ".*nodeStatusPlotSmall*\\.rds$")
 
 # Process the files and extract metadata
-plot_small_data <- plot_small_files %>%
+nodes_status_plots <- 
+  node_status_plots_files %>%
   map(read_rds_file_metadata)
 
 # Save the combined list of plots and metadata
-write_rds(plot_small_data, "combined_plot_small_data.rds", compress = 'xz')
+write_rds(nodes_status_plots, "node_status_plots.rds", compress = 'xz')
 
+#######Indutrsy plots########
+
+# List .rds files containing "industry" in their names
+industry_files <- dir_ls(folder_path, recurse = TRUE, regexp = ".*industry.*\\.rds$")
+
+# Process the files and extract metadata
+industry_plots <- 
+  industry_files %>%
+  map(read_rds_file_metadata)
+
+# Save the combined list of plots and metadata
+write_rds(industry_plots, "industry_plots.rds", compress = 'xz')
